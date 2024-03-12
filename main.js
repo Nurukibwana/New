fd6003cd39dac58a0788f3774dd06c0840fe92fb
Nurukibwana <nurukibwana996@gmail.com>
2024-03-13 01:23:14 +0300

    update

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log( "Hello World!" );
+
+ScrollReveal({
+ reset: true,
+ distance: '80px',
+ duration: 1500,
+ delay: 200
+ });
+ 
+ ScrollReveal().reveal('.form, .sms,  .bod, .end p, .header a', { origin: 'top'});
+ 
+// const btn = document.querySelector('btn');
+ 
+// btn.addEventListener('click', ()=>{
+//   btn.document.classList('.active');
+// });
+
+function btnclick() {
+  alert ('hello');
+};
